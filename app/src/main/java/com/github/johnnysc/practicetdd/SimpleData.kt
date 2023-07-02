d3c99e875b96e32e9d94287bf00f3d8807dc42ee
Ilya Zhigaras <zhigaras.dev@gmail.com>
2023-07-03 04:42:44 +0600

    task-005 solved

--- app/src/main/java/com/github/johnnysc/practicetdd/SimpleData.kt
+++ app/src/main/java/com/github/johnnysc/practicetdd/SimpleData.kt
@@ -1,6 +1,36 @@
 package com.github.johnnysc.practicetdd
 
+import android.os.Build
+import androidx.annotation.RequiresApi
+
 /**
  * @author Asatryan on 26.12.2022
  */
 data class SimpleData(private val value: String)
+
+interface Now {
+    
+    fun now(): Long
+}
+
+interface CacheDataSource {
+    
+    fun add(item: SimpleData)
+    
+    fun data(): List<SimpleData>
+    
+    @RequiresApi(Build.VERSION_CODES.N)
+    class Timed(private val now: Now, private val lifeTimeMillis: Long): CacheDataSource {
+    
+        private val data = mutableListOf<Pair<Long, SimpleData>>()
+        
+        override fun add(item: SimpleData) {
+            data.add(now.now() to item)
+        }
+    
+        override fun data(): List<SimpleData> {
+            data.removeIf { it.first + lifeTimeMillis < now.now() }
+            return data.map { it.second }
+        }
+    }
+}
