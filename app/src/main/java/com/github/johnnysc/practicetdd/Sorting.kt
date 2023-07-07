9221b027b798d3911dc7430c0419ca77ac612a83
Ilya Zhigaras <zhigaras.dev@gmail.com>
2023-07-08 04:10:59 +0600

    task-009 solved

---
+++ app/src/main/java/com/github/johnnysc/practicetdd/Sorting.kt
@@ -0,0 +1,27 @@
+package com.github.johnnysc.practicetdd
+
+interface Sorting {
+    
+    fun sort(list: List<Int>): List<Int>
+    
+    class Base(private val forOut: For, private val forIn: For) : Sorting {
+        
+        override fun sort(list: List<Int>): List<Int> {
+            val sortedList = list.toMutableList()
+            forOut.repeat(sortedList.size) {
+                var changesCount = 0
+                forIn.repeat(sortedList.size - 1) {
+                    if (sortedList[it] > sortedList[it + 1]) {
+                        val left = sortedList[it]
+                        sortedList[it] = sortedList[it + 1]
+                        sortedList[it + 1] = left
+                        changesCount++
+                    }
+                    false
+                }
+                changesCount == 0
+            }
+            return sortedList
+        }
+    }
+}
