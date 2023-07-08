7b0ec1a98424551c39f80ec0b37f6f2168832327
Ilya Zhigaras <zhigaras.dev@gmail.com>
2023-07-08 19:19:20 +0600

    task-11 solved

---
+++ app/src/main/java/com/github/johnnysc/practicetdd/Count.kt
@@ -0,0 +1,21 @@
+package com.github.johnnysc.practicetdd
+
+interface Count {
+    
+    fun click()
+    
+    class Base(private val callback: Callback) : Count {
+    
+        private var count = 0
+        
+        override fun click() {
+            count++
+            callback.provide(count.toString())
+        }
+    }
+    
+    interface Callback {
+    
+        fun provide(value: String)
+    }
+}
